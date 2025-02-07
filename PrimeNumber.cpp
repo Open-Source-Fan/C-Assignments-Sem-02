2dd750ea8e89ff80b5457258a4a920ef1e7249b4
Open-Source-Fan <hackertyagi9@gmail.com>
2025-02-07 17:03:02 +0530

    PrimeNumber.cpp

--- PrimeNumber.cpp
+++ PrimeNumber.cpp
@@ -9,13 +9,11 @@ int main(){
 
     if (n <= 1) {
         cout << n << " is NOT a Prime Number." << endl;
-        return 0;  // Directly exit if n <= 1
     }
 
     for (i = 2; i < n; i++) {
         if (n % i == 0) {
             cout << n << " is NOT a Prime Number." << endl;
-            return 0;  // Exit as soon as a factor is found
         }
     }
 
